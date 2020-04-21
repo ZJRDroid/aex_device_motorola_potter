c527d189916a504151657edb10e282878c99a7e8
ZJRDroid <priv.zjr@gmail.com>
2020-04-20 21:47:50 -0300

    potter: AEX Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_potter.mk
+    $(LOCAL_DIR)/aosp_potter.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_potter-user \
-    ancient_potter-userdebug \
-    ancient_potter-eng
+    aosp_potter-user \
+    aosp_potter-userdebug \
+    aosp_potter-eng
